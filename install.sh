9846f42328b4061112715b0f5a55a5488fd81373
Costas Kirgoussios <ckir1508@hotmail.com>
2016-03-01 12:39:59 +0200

    Initial commit

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 set -e
 set -o pipefail
 
-pip install --upgrade pip
 mkdir venv
 pip install virtualenv
 virtualenv venv
 source ./venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 
